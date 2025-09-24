5e335bd5f9d31e3b9194f2095fb5e5e55f37f901
Z-1020 <zoemargerie14@gmail.com>
2025-09-24 20:47:12 +0200

    feat: ajout de la saisie clavier pour déplacer la raquette

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,8 @@
 
 let canvas = document.getElementById('jeu')
 let ctx = canvas.getContext('2d')
+let direction = document.querySelector('bouton_direction')
+let raquetteX = 150;
 
 let frame_requester;
 
@@ -23,13 +25,13 @@ const timer = setInterval(() =>{
     
     
         
-},100);
+});
 
 document.getElementById("nouvelle_partie").onclick = function lancer(){
     score=0;
 
     état="en cours"
-
+    raquetteX = canvas.width/2 - (12.5/100)*canvas.width;
     ball_x=200
     ball_y=535
 
@@ -39,15 +41,58 @@ document.getElementById("nouvelle_partie").onclick = function lancer(){
     
 }
 
-/*création de la raquette*/ 
+/*création de la raquette*/
 
-ctx.fillStyle = '#ffffffff';
-ctx.fillRect(150, 550, 100, 10);
-ctx.fillStyle = 'rgba(73, 209, 250, 1)';
-ctx.arc(ball_x,ball_y,15,0,2*Math.PI)
-ctx.fill();
+function raquette(){
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    ctx.fillStyle = '#ffffffff';
+    ctx.fillRect(raquetteX, (140/100)*canvas.width, (25/100)*canvas.width, (2/100)*canvas.width);
+}
+
+/*création de la ball*/
+
+function ball(){
+    ctx.fillStyle = 'rgba(73, 209, 250, 1)';
+    ctx.arc(ball_x,ball_y,15,0,2*Math.PI)
+    ctx.fill();
+}
+
+raquette();
+ball();
 
 
+/*controle clavier de la raquette*/
+
+document.addEventListener("keydown", (e)=>{
+    switch (e.key){
+        case "ArrowLeft":
+            if(raquetteX>0) raquetteX -= 10;
+            break;
+        case "ArrowRight":
+            if(raquetteX +(25/100)*canvas.width < canvas.width) raquetteX += 10;
+            break;
+        default:
+            break;
+    }
+    raquette();
+    ball();
+})
+
+direction.addEventListener("click", (f)=>{
+    switch (direction.getAttribute("value")){
+        case "gauche":
+            if(raquetteX>0) raquetteX -= 10;
+            break;
+        case "droit":
+           if(raquetteX +(25/100)*canvas.width < canvas.width) raquetteX += 10;
+            break;
+        default:
+            break;
+    }
+    raquette();
+    ball(); 
+})
 
 
 function boucle(){
@@ -58,9 +103,7 @@ function boucle(){
 }
 
 function updateFrame(){
-    ctx.clearRect(0,0,canvas.width, canvas.height)
-    ctx.fillStyle = '#ffffffff';
-    ctx.fillRect(150, 550, 100, 10);
+    raquette();
     ctx.fillStyle = 'rgba(73, 209, 250, 1)';
     ctx.beginPath()
     ctx.arc(ball_x,ball_y,15,0,2*Math.PI)
