33aed126016f1e20d8474b957a73c862719c1c6d
Z-1020 <zoemargerie14@gmail.com>
2025-09-24 21:43:48 +0200

    feat: ajout du controle de la raquette avec les boutons

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,44 +1,46 @@
 
-let canvas = document.getElementById('jeu')
-let ctx = canvas.getContext('2d')
-let direction = document.querySelector('bouton_direction')
+/*------- Déclaration de variable -------*/
+
+//canvas
+let canvas = document.getElementById('jeu');
+let ctx = canvas.getContext('2d');
+// raquette
+let gauche = document.getElementById('gauche');
+let droit = document.getElementById('droit');
 let raquetteX = 150;
-
+//balle
 let frame_requester;
-
 let score = 0;
-
 let état = "stop";
-
-/*coordonnées et variables de la balle*/
-let orientation_
-let ball_x = 200
-let ball_y = 535
-let vitesse = 1
-
+//coordonnées et variables de la balle
+let orientation_;
+let ball_x = 200;
+let ball_y = 535;
+let vitesse = 1;
 const timer = setInterval(() =>{
     if(état=="en cours"){
         score = score+0.1;
         let texte = score.toFixed(0);
         document.getElementById("score").textContent="Score : "+texte;
-    }
-    
-    
-        
-});
+    }     
+},100);
+
+/*------- fin de déclaration de variable -------*/
+
+
+/*------- Déclaration de fonction -------*/
 
 document.getElementById("nouvelle_partie").onclick = function lancer(){
     score=0;
 
-    état="en cours"
+    état="en cours";
     raquetteX = canvas.width/2 - (12.5/100)*canvas.width;
-    ball_x=200
-    ball_y=535
+    ball_x=200;
+    ball_y=535;
 
     orientation_ = Math.random()*360;
 
-    boucle()
-    
+    boucle();
 }
 
 /*création de la raquette*/
@@ -54,23 +56,50 @@ function raquette(){
 
 function ball(){
     ctx.fillStyle = 'rgba(73, 209, 250, 1)';
-    ctx.arc(ball_x,ball_y,15,0,2*Math.PI)
+    ctx.arc(ball_x,ball_y,15,0,2*Math.PI);
     ctx.fill();
 }
 
-raquette();
-ball();
+function boucle(){
+    cancelAnimationFrame(frame_requester);
+    updateGame();
+    updateFrame();
+    frame_requester=requestAnimationFrame(boucle);
+}
 
+function updateFrame(){
+    raquette();
+    ctx.fillStyle = 'rgba(73, 209, 250, 1)';
+    ctx.beginPath();
+    ctx.arc(ball_x,ball_y,15,0,2*Math.PI);
+    ctx.fill();
+    console.log(canvas.width);
+}
+
+function updateGame(){
+    ball_x=ball_x+Math.sin(orientation_)*vitesse;
+    ball_y=ball_y+Math.cos(orientation_)*vitesse;
+}
+
+
+/*------- fin de Déclaration de fonction -------*/
+
+
+/*------- Début du programme -------*/
+
+raquette();
+
+ball();
 
 /*controle clavier de la raquette*/
 
 document.addEventListener("keydown", (e)=>{
     switch (e.key){
         case "ArrowLeft":
-            if(raquetteX>0) raquetteX -= 10;
+            if(raquetteX>0) raquetteX -= 20;
             break;
         case "ArrowRight":
-            if(raquetteX +(25/100)*canvas.width < canvas.width) raquetteX += 10;
+            if(raquetteX +(25/100)*canvas.width < canvas.width) raquetteX += 20;
             break;
         default:
             break;
@@ -78,40 +107,20 @@ document.addEventListener("keydown", (e)=>{
     raquette();
     ball();
 })
+/* utilise le bouton gauche pour déplacer la raquette */
 
-direction.addEventListener("click", (f)=>{
-    switch (direction.getAttribute("value")){
-        case "gauche":
-            if(raquetteX>0) raquetteX -= 10;
-            break;
-        case "droit":
-           if(raquetteX +(25/100)*canvas.width < canvas.width) raquetteX += 10;
-            break;
-        default:
-            break;
-    }
+gauche.addEventListener("click", ()=>{
+    
+            if(raquetteX>0) raquetteX -= 20;
     raquette();
     ball(); 
 })
 
+/* utilise le bouton droit pour déplacer la raquette */
 
-function boucle(){
-    cancelAnimationFrame(frame_requester)
-    updateGame()
-    updateFrame()
-    frame_requester=requestAnimationFrame(boucle)
-}
-
-function updateFrame(){
+droit.addEventListener("click", ()=>{
+    if(raquetteX +(25/100)*canvas.width < canvas.width) raquetteX += 20;
     raquette();
-    ctx.fillStyle = 'rgba(73, 209, 250, 1)';
-    ctx.beginPath()
-    ctx.arc(ball_x,ball_y,15,0,2*Math.PI)
-    ctx.fill();
-    console.log(canvas.width)
-}
-
-function updateGame(){
-    ball_x=ball_x+Math.sin(orientation_)*vitesse
-    ball_y=ball_y+Math.cos(orientation_)*vitesse
-}
+    ball(); 
+})
+/*------- fin du programme -------*/
